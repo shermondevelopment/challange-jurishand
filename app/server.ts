bf9764aaadd71e381a88399902b7067f65b7fd60
shermondevelopment <victorshermon@yahoo.com>
2023-04-01 13:28:56 -0300

    docs: create docs with swagger

--- app/server.ts
+++ app/server.ts
@@ -6,8 +6,13 @@ import express, { json } from 'express'
 /* router */
 import articleRouter from './routers/article-router'
 
+/* swagger */
+import swaggerDoc from 'swagger-ui-express'
+import swaggerRules from '../swagger.json'
+
 const app = express()
 app.use(json())
 app.use(articleRouter)
+app.use('/docs', swaggerDoc.serve, swaggerDoc.setup(swaggerRules))
 
 app.listen(3001, () => console.log(`app running in port ${process.env.PORT}`))
