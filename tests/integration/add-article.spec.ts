dad9847bf5ce1415f543294642fe2ac31b99bf03
shermondevelopment <victorshermon@yahoo.com>
2023-04-01 16:03:27 -0300

    test: should get an error if the user doesn't provide a author

--- tests/integration/add-article.spec.ts
+++ tests/integration/add-article.spec.ts
@@ -12,4 +12,14 @@ describe('Creating article', () => {
     expect(response.statusCode).toBe(422)
     expect(response.body).toEqual({ error: '"title" is required' })
   })
+  it("should get an error if the user doesn't provide a author", async () => {
+    const response = await supertest(app).post('/add').send({
+      title: 'any_title',
+      content: 'any_content',
+      date: new Date(),
+      category: 'any_category'
+    })
+    expect(response.statusCode).toBe(422)
+    expect(response.body).toEqual({ error: '"author" is required' })
+  })
 })
