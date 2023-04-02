ee7fd8799f1a2aabab1e8654cfeea690d7d57594
shermondevelopment <victorshermon@yahoo.com>
2023-04-02 11:56:39 -0300

    feat: generating report of articles by author

--- scripts/reports/main.py
+++ scripts/reports/main.py
@@ -1,7 +1,6 @@
 import requests
 import json
 import pandas as pd
-import os.path
 
 
 getArticles = requests.get('http://localhost:3001/')
@@ -9,8 +8,14 @@ getArticles = requests.get('http://localhost:3001/')
 # transform json to dataframe
 df = pd.read_json(json.dumps(getArticles.json()))
 
+# quantity of category by articles
 categoryCountReports = df.groupby(['category']).agg(
     quantity_category_by_article=('category', 'count')
 )
-print(os.path.abspath("nmae"))
+
+authorCount = df.groupby(['author']).agg(
+    quantity_author_by_article=('category', 'count')
+)
+
 categoryCountReports.to_csv('scripts/reports/category.csv')
+authorCount.to_csv('scripts/reports/author.csv')
